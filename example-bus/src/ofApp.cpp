93ac2e1b14a8e85dd5183c8996a14ca2ec6ba34e
nariakiiwatani <nariakiiwatani@annolab.com>
2019-05-22 18:01:21 +0900

    [bus] add bus protocol

---
+++ example-bus/src/ofApp.cpp
@@ -0,0 +1,93 @@
+#include "ofApp.h"
+
+using namespace ofx::nng;
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	bus_.resize(4);
+	Bus::Settings buss;
+	for(int i = 0; i < bus_.size(); ++i) {
+		auto &b = bus_[i];
+		b = std::make_shared<Bus>();
+		b->setup(buss, std::function<void(const ofBuffer&)>([i](const ofBuffer &buffer) {
+			cout << "node" << i << ": receive:" << buffer.getText() << endl;
+		}));
+		std::string recv_url = "inproc://bus"+ofToString(i);
+		b->listen(recv_url);
+		for(int j = i+1; j < bus_.size(); ++j) {
+			std::string send_url = "inproc://bus"+ofToString(j);
+			b->dial(send_url);
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	int index = key-'1';
+	if(index >= 0 && index < bus_.size()) {
+		ofBuffer data;
+		data.set("message from node"+ofToString(index));
+		bus_[key-'1']->send(data);
+		cout << "node" << index << ": send" << endl;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofxNNGBus.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "nng.h"
+#include "protocol/bus0/bus.h"
+#include "ofxNNGNode.h"
+#include "ofxNNGParseFunctions.h"
+#include "ofxNNGConvertFunctions.h"
+
+namespace ofx {
+namespace nng {
+class Bus : public Node
+{
+public:
+	struct Settings {
+		bool allow_callback_from_other_thread=false;
+	};
+	template<typename T>
+	bool setup(const Settings &s, const std::function<void(const T&)> &callback) {
+		int result;
+		result = nng_bus_open(&socket_);
+		if(result != 0) {
+			ofLogError("ofxNNGBus") << "failed to open socket; " << nng_strerror(result);
+			return false;
+		}
+		callback_ = [callback](nng_msg *msg) {
+			callback(util::parse<T>(msg));
+		};
+		async_ = s.allow_callback_from_other_thread;
+		if(!async_) {
+			ofAddListener(ofEvents().update, this, &Bus::update);
+		}
+		nng_aio_alloc(&aio_, &Bus::receive, this);
+		nng_recv_aio(socket_, aio_);
+		return true;
+	}
+	template<typename T>
+	bool send(const T &data, bool blocking=false) {
+		nng_msg *msg;
+		nng_msg_alloc(&msg, 0);
+		if(!util::convert(data, msg)) {
+			ofLogError("ofxNNGBus") << "failed to convert message";
+			return false;
+		}
+		int result;
+		int flags = 0;
+		if(!blocking) flags |= NNG_FLAG_NONBLOCK;
+		result = nng_sendmsg(socket_, msg, flags);
+		if(result != 0) {
+			ofLogError("ofxNNGBus") << "failed to send message; " << nng_strerror(result);
+			nng_msg_free(msg);
+			return false;
+		}
+		return true;
+	}
+	~Bus() {
+		if(aio_) nng_aio_free(aio_);
+	}
+private:
+	nng_aio *aio_;
+	std::function<void(nng_msg*)> callback_;
+	bool async_;
+	ofThreadChannel<nng_msg*> channel_;
+	static void receive(void *arg) {
+		auto me = (Bus*)arg;
+		auto result = nng_aio_result(me->aio_);
+		if(result != 0) {
+			ofLogError("ofxNNGBus") << "failed to receive message; " << nng_strerror(result);
+			return;
+		}
+		auto msg = nng_aio_get_msg(me->aio_);
+		if(me->async_) {
+			me->callback_(msg);
+			nng_msg_free(msg);
+		}
+		else {
+			me->channel_.send(msg);
+		}
+		nng_recv_aio(me->socket_, me->aio_);
+	}
+	void update(ofEventArgs&) {
+		nng_msg *msg;
+		while(channel_.tryReceive(msg)) {
+			callback_(msg);
+			nng_msg_free(msg);
+		}
+	}
+};
+}}
