ce5d9b85e3ae46a1dee843691f494b6411409a4c
nariakiiwatani <nariakiiwatani@annolab.com>
2020-03-18 16:50:51 +0900

    don't need comma after OFX_NNG_NOTIFY_USE_MEMCPY()

--- src/detail/user_optimization.h
+++ src/detail/user_optimization.h
@@ -7,5 +7,5 @@ namespace ofxNNG {
 	template<typename T> struct is_safe_to_use_memcpy<T, typename std::enable_if<T::is_safe_to_use_memcpy>::type> : std::true_type{};
 }
 
-#define OFX_NNG_NOTIFY_TO_USE_MEMCPY(Type) template<> struct ofxNNG::is_safe_to_use_memcpy<Type> : std::true_type{}
+#define OFX_NNG_NOTIFY_TO_USE_MEMCPY(Type) template<> struct ofxNNG::is_safe_to_use_memcpy<Type> : std::true_type{};
 #define OFX_NNG_NOTIFY_TO_USE_MEMCPY_MEMBER static const bool is_safe_to_use_memcpy=true;
