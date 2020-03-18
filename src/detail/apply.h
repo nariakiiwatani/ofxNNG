d73d1e02ef29a92b303934bf1a2560d6d8fc9cdd
nariakiiwatani <nariakiiwatani@annolab.com>
2020-03-18 16:28:13 +0900

    fixed apply for single ofxNNG::Message conversion

--- src/detail/apply.h
+++ src/detail/apply.h
@@ -12,10 +12,25 @@ namespace ofxNNG {
 		-> decltype(tuple_apply_impl(std::forward<F>(func), std::forward<T>(t), nlohmann::detail::make_index_sequence<N>{})) {
 			return tuple_apply_impl(std::forward<F>(func), std::forward<T>(t), nlohmann::detail::make_index_sequence<N>{});
 		}
+		template<typename F, typename ...Args>
+		struct apply_fun {
+			auto operator()(F &&func, const ofxNNG::Message &msg)
+			-> decltype(func(declval<Args>()...)) {
+				return detail::tuple_apply(func, msg.get<std::tuple<Args...>>());
+			}
+		};
+		template<typename F>
+		struct apply_fun<F, ofxNNG::Message> {
+			auto operator()(F &&func, const ofxNNG::Message &msg)
+			-> decltype(func(declval<ofxNNG::Message>())) {
+				return func(ofxNNG::Message(msg));
+			}
+		};
 	}
+
 	template<typename ...Args, typename F>
 	static auto apply(F &&func, const ofxNNG::Message &msg)
-	-> decltype(func(declval<Args>()...)) {
-		return detail::tuple_apply(func, msg.get<std::tuple<Args...>>());
+	-> decltype(declval<detail::apply_fun<F, Args...>>()(func, msg)) {
+		return detail::apply_fun<F, Args...>()(func, msg);
 	}
 }
